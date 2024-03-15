c3ba8dcf22370de7b2bd54758f755846f9f1b44c
Arya Mohanan <arya.mohanan@ibm.com>
2024-03-15 10:13:52 +0530

    feat: added support for client esm app

--- requireHook.js
+++ requireHook.js
@@ -1,33 +1,44 @@
 const Module = require('module');
-
+const path = require('path');
 const originalLoad = Module._load;
 
 let instrumentedModules = [];
 
 function initHook(moduleName, instrument) {
-  if (typeof moduleName !== 'string') {
-    throw new Error(`moduleName must be a string, received ${moduleName}`);
-  }
-  if (typeof instrument !== 'function') {
-    throw new Error(`instrument must be a function, received ${instrument}`);
+  if (typeof moduleName !== 'string' || typeof instrument !== 'function') {
+    throw new Error(
+      `moduleName must be a string and instrument must be a function`
+    );
   }
   instrumentedModules.push({ moduleName, instrument });
 }
-function hookModuleLoad(moduleName) {
+
+function hookModuleLoad(moduleName, parent) {
+  const orgModuleName = moduleName;
+  // Extracting the module name from the path for ESM modules
+  if (
+    path.isAbsolute(moduleName) &&
+    ['.node', '.json', '.ts'].indexOf(path.extname(moduleName)) === -1
+  ) {
+    const match = moduleName.match(/node_modules\/((@.*?\/.*?)|(.*?))(?=\/|$)/);
+
+    if (match && match[1]) {
+      moduleName = match[1];
+    }
+  }
   for (const {
     moduleName: targetModuleName,
     instrument,
   } of instrumentedModules) {
     if (moduleName === targetModuleName) {
       // Load the module via the original Module._load implementation.
-      const originalModule = originalLoad.apply(Module, arguments);
-      // Apply the instrument function to the loaded module and return the result.
+      const originalModule = originalLoad.call(Module, orgModuleName, parent);
       return instrument(originalModule);
     }
   }
 
   // Forward the call to the original module loading implementation if this is not the targeted module.
-  return originalLoad.apply(Module, arguments);
+  return originalLoad.call(Module, orgModuleName, parent);
 }
 
 exports.initHook = initHook;
