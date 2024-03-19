3d94fc1b9cae720134ceaf6b1b1cf3ba2cef01b3
Arya Mohanan <arya.mohanan@ibm.com>
2024-03-19 11:10:46 +0530

    added more instrumentations

--- tracer.js
+++ tracer.js
@@ -1,9 +1,8 @@
 'use strict';
 
 const requireHook = require('./requireHook.js');
-
-const spans = [];
-const methods = ['get', 'post'];
+const { instrument: superagent } = require('./instrumentations/superagent.js');
+const { instrument: got } = require('./instrumentations/got.js');
 const Span = {
   module: '',
   data: {},
@@ -11,39 +10,9 @@ const Span = {
   date: null,
 };
 
-exports.init = function () {
-  requireHook.initHook('got', instrument);
-};
-
-function instrument(orgModule) {
-  if (orgModule) {
-    methods.forEach((method) => {
-      if (typeof orgModule[method] === 'function') {
-        const originalMethod = orgModule[method];
-        orgModule[method] = function () {
-          Span.module = orgModule.name;
-          Span.data = {
-            url: arguments[0],
-            method: method
-          };
-          Span.date = new Date().toISOString();
-
-          const request = originalMethod.apply(this, arguments);
-
-          return request
-            .then(() => {
-              spans.push({ ...Span });
-              console.log('collected trace data', spans);
-            })
-            .catch((err) => {
-              Span.error = err.toString();
-              spans.push({ ...Span });
-              console.log('spans', spans);
-            });
-        };
-      }
-    });
-  }
-
-  return orgModule;
+function init() {
+  requireHook.initHook('superagent', superagent);
+  requireHook.initHook('got', got);
 }
+
+module.exports = { Span, init };
